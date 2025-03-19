8e15d729aa49ac64e34203233594b81578e8cfc9
grafitosLeg <wawrzyniakrafal74@gmail.com>
2025-03-19 19:35:58 +0100

    Dodano #12

---
+++ Dane_analizy_lista_max_min#12.py
@@ -0,0 +1,8 @@
+# obliczanie maksimum i minimum
+# kod oblicza maksymalną i minimalną wartość z listy liczb
+# moze byc przydatny w analizie danych , gdzie ważne jest znalezienie wartości skrajnych
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (f"Maksimum: {max(lista)}")
+print (f"Minimum: {min(lista)}")
+
