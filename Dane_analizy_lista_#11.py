248f844a1fa2941e26e18530d3190f389706f105
grafitosLeg <wawrzyniakrafal74@gmail.com>
2025-03-19 19:29:01 +0100

    Dodano #11 Trick

---
+++ Dane_analizy_lista_#11.py
@@ -0,0 +1,10 @@
+# sprawdzanie , czy element jest w liscie
+# kod sprawdza czy element jest w liscie. Używa operatora "in",aby zwrócić wartość True lub False
+#co moze być przydatne do walidacji danych, filtracji lub wykonywania warunkowych operacji na listach w programach
+ 
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(2 in lista) # True
+print(10 in lista) # False
+
