485feb588bdc00cbb8779e82609983c971be0606
grafitosLeg <wawrzyniakrafal74@gmail.com>
2025-03-21 13:28:42 +0100

    #15 przedebugowano z Github Copilot, zrozumienie działania funkcji

--- Dane_analizy_czy_jest_liczba_pierwsza_#15.py
+++ Dane_analizy_czy_jest_liczba_pierwsza_#15.py
@@ -3,17 +3,18 @@
 # następnie w pętli wypisuje wszystkie liczby pierwsze w przedziale od 2 do 19
 # Może byc przydatny w matematyce grach losowych lub algorytmach wymagających analizy liczb pierwszych
 
-# def jest_pierwsza(n):
-#     if n < 2:
-#         return False
-#     for i in range(2, int(n**0.5) + 1):
-#         if n % i == 0:
-#             return False
-#     return True
+def jest_pierwsza(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-# for i in range(2, 20):
-#     if jest_pierwsza(i):
-#         print(i)
+for i in range(2, 20):
+    if jest_pierwsza(i):
+        print(i)
+        
         
 #wersja copilot z logami
 # def jest_pierwsza(n):
