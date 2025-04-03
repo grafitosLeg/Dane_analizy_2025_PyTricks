651086fbd3e6f4abffaa4454db71cd7bb4fe63ac
grafitosLeg <wawrzyniakrafal74@gmail.com>
2025-04-03 19:29:06 +0200

    Dodano #26

---
+++ Dane_analizy_zbior_#26.py
@@ -0,0 +1,10 @@
+# dodawanie eementów do zbioru
+# kod ten ilustruje, jak dodawać elementy do zbioru w Pythonie. 
+# w przykłądzie dodano liczbę 3 do zbioru zawierającego 1 i 2
+#, a nsatępnie wyswietlono zawartość zbioru.
+# Przydatne w sytuacjach, gdy potrzebujemy przechowywać unikalne wartości i dynamicznie je modyfikować
+
+
+zbiór = {1, 2}
+zbiór.add(3)
+print(zbiór)
